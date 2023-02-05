66f31558d8232f78d0d50d8286e325a591137f36
Charles Crawford <crawford.charles.g@gmail.com>
2023-02-04 18:32:50 -0700

    Create aws-p2-ubuntu-gpu-setup-script.sh

---
+++ aws-p2-ubuntu-gpu-setup-script.sh
@@ -0,0 +1,19 @@
+# reboot needed for nvidia driver to start working and the docker group will also be added to the ubuntu user
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install xorg -y
+sudo apt-get install -y nvidia-driver-460
+sudo apt-get update
+sudo apt-get install -y ca-certificates     curl     gnupg     lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+export distribution=$(. /etc/os-release;echo $ID$VERSION_ID)       && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add -       && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo systemctl restart docker
+sudo usermod -a -G docker ubuntu
+sudo reboot
